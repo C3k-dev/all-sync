862149a77d141faf7751a2a2753197d9851e05a4
C3k-dev <work.culture3k@gmail.com>
2025-09-04 15:30:37 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
+    env: {
+      MONGO_USER: process.env.MONGO_USER,
+      MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+      MONGO_CLUSTER: process.env.MONGO_CLUSTER,
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    },
     experimental: {
     serverActions: {
     bodySizeLimit: '50mb'
